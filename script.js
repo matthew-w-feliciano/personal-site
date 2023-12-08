20100f70ba92c19fee954723b537659e303c4e1e
matthew-w-feliciano <152580593+matthew-w-feliciano@users.noreply.github.com>
2023-12-08 13:20:41 -0500

    added css for nav menu

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Smooth scroll behavior for anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
         e.preventDefault();
